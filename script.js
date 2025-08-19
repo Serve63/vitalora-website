4c04ee5a95dc0fba51f339154242788a455fa3c6
Servé Creusen <servecreusen@MacBook-Air-van-Serve.local>
2025-08-19 15:45:34 +0200

    Update version parameters to v=live2 and clean up tracer code

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-// Kill service worker cache
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.getRegistrations().then(rs => rs.forEach(r => r.unregister()));
-}
 
 // Smooth scrolling for anchor links
 document.addEventListener('DOMContentLoaded', function() {
