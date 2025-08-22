5456a6c2dfcfa3c415dc047e1acf8a7c4e988baf
Servé Creusen <servecreusen@MacBook-Air-van-Serve.local>
2025-08-22 14:27:25 +0200

    Change access code to 225000

--- academy-login.js
+++ academy-login.js
@@ -1,6 +1,6 @@
 (function() {
   const ACCESS_KEY = 'vitalora_academy_access';
-  const VALID_CODE = '22500';
+  const VALID_CODE = '225000';
 
   function qs(sel) { return document.querySelector(sel); }
 
